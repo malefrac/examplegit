e7a994c4bccbbe601f83619a90a0806b1805c063
Mayra Alejandra <malefrac@gmail.com>
2023-09-18 10:40:54 -0500

    Creación de archivo Exaple1.py
    Se crea ejemplo de números aleatorios en Python.

---
+++ Example_python/Example1.py
@@ -0,0 +1,29 @@
+"""
+Fecha: 18/09/2023
+Autor: Mayra Alejandra Franco Castaño
+Objetivo: Ejemplo de versionamiento con git desde python
+"""
+import random
+
+random_number = random.randint(1, 10) #Genera núms aleatorios entre un rango
+print(random_number)
+
+print("-----------------------------------")
+
+for i in range(0, 10) :
+    random_number = random.randrange(20, 100, 5) #Genera núms aleatorios entre un rango multiplos de 5
+    print(random_number)
+
+print("-----------------------------------")
+
+for i in range(0, 10) :
+    random_number = random.random() #Genera núms aleatorios con decimales
+    print(random_number)
+
+print("-----------------------------------")
+
+for i in range(0, 10) :
+    random_number = random.uniform(10.5, 20.6) #Genera núms aleatorios partiendo de decimales
+    print(random_number)
+
+print("-----------------------------------")
